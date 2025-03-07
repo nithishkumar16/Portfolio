3f6f40474cbf04ae3184aaa52b0b3249b95cbf62
nithish <nithish6@gmail.com>
2025-03-06 19:30:50 -0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,150 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
+
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+window.onscroll = function () {
+  headerShadow();
+};
+
+function headerShadow() {
+  const navHeader = document.getElementById("header");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
+  } else {
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
+  }
+}
+
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText", {
+  strings: ["Software Developer", "AI Enthusiast"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000,
+});
+
+/*------download resume button------*/
+function downloadResume() {
+  const fileId = "17mx4mYLyaz2JAs0lYG9ocltkIDdHzOfN";
+  const directDownloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+  window.open(directDownloadUrl, "_blank");
+}
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/* -- HOME -- */
+sr.reveal(".featured-text-card", {});
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".featured-text-info", { delay: 200 });
+sr.reveal(".featured-text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 300 });
+
+/* -- PROJECT BOX -- */
+sr.reveal(".project-box", { interval: 200 });
+
+/* -- HEADINGS -- */
+sr.reveal(".top-header", {});
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: "left",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+srLeft.reveal(".about-info", { delay: 100 });
+srLeft.reveal(".contact-info", { delay: 100 });
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: "right",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+srRight.reveal(".skills-box", { delay: 100 });
+srRight.reveal(".form-control", { delay: 100 });
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+function sendEmail() {
+  var name = document.getElementById("name").value.trim();
+  var email = document.getElementById("email").value.trim();
+  var message = document.getElementById("message").value.trim();
+
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (name === "" || email === "" || message === "") {
+    alert("Please fill in all fields before sending the message.");
+    return;
+  }
+
+  if (!emailPattern.test(email)) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+
+  emailjs.send("service_chp9sus", "template_as1uytb", {
+    name: name,
+    email: email,
+    message: message,
+  }, "05nSpC-n8PfAv2RFh")
+    .then(function (response) {
+      alert("Email sent successfully!");
+      document.getElementById("contact-form").reset(); // Clear form after submission
+    }, function (error) {
+      alert("Failed to send email. Please try again later.");
+      console.error("Error:", error);
+    });
+}
+
+
+window.addEventListener("scroll", scrollActive);
